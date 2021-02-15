b5f7259ddb7ab00f01f21224439ba0997afddac9
rafitc <rafirasheed.tc99@gmail.com>
2021-02-15 22:33:41 +0530

    completed main file

--- python/read.py
+++ python/read.py
@@ -1,13 +1,62 @@
 import serial
+import sys
+import glob
 import hello as time
 import csv
 count = 0
 
-ser = serial.Serial('/dev/tty.usbmodem14201', '9600')
+def serial_ports():
+    """ Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+port = serial_ports()
+i = 1
+for p in port:
+    print(i,")",port)
+    i=i+1
+
+port_number = input("Choose your port No: ")
+port_number = int(port_number)
+if(port_number<i):
+    print("Selected ", port[port_number-1])
+else:
+    print("Wrong port number choosed")
+    exit()
+PORT = port[port_number-1]
+
+try:
+    ser = serial.Serial(PORT, '9600')
+    print("PORT STARTED")
+except SerialException:
+    print("Serial port error!!")
+
 ser.flushInput()
 
 with open(time.fileName(), 'w') as file:
-    
     writer = csv.writer(file)
     ser_bytes = ser.readline()
     print(ser_bytes.decode('utf-8'))
@@ -28,5 +77,3 @@ with open(time.fileName(), 'w') as file:
             ser.close()
             count = 0
             break
-    
-
